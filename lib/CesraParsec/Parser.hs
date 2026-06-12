fec50e59eb01caac8029435c5ba73bc2c5984149
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2026-06-12 18:52:57 +0300

    initial parsing PoC

--- lib/CesraParsec/Parser.hs
+++ lib/CesraParsec/Parser.hs
@@ -1,8 +1,18 @@
 module CesraParsec.Parser where
 
-import Text.Parsec.Text (Parser)
+import qualified Data.List.NonEmpty as NE
+import Text.Parsec
+--FIXME: String -> Text
+import Text.Parsec.String
+import qualified Text.Parsec.Char as P
 
 import CesraParsec.Types
 
-toParser :: Cesra a -> Parser a
-toParser = undefined
+toParser :: Cesra String -> Parser String
+toParser (Cesra c's) = foldr1 (<>) $ fmap primeToParser c's
+
+primeToParser :: Cesra' String -> Parser String
+primeToParser (One a) = P.string a
+primeToParser (Many a) = undefined --many1 $ P.string a
+primeToParser (Choice a) = choice . NE.toList $ NE.map primeToParser a
+primeToParser (Optional a) = option "" $ primeToParser a
--- lib/CesraParsec/Types.hs
+++ lib/CesraParsec/Types.hs
@@ -3,5 +3,7 @@ module CesraParsec.Types where
 import Data.List.NonEmpty (NonEmpty)
 
 data Cesra a = Cesra (NonEmpty (Cesra' a))
+  deriving Show
 
 data Cesra' a = One a | Many a | Choice (NonEmpty (Cesra' a)) | Optional (Cesra' a)
+  deriving Show
